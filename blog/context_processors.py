e9c9bbea945134b9acb6e6c238b5ef7e9c2b0340
Trebo Christoffer <christoffer.trebo@gmail.com>
2021-02-22 20:11:31 +0100

    added login

--- blog/context_processors.py
+++ blog/context_processors.py
@@ -2,6 +2,7 @@
 from .models import Blog, BlogComment, BlogAuthor
 
 def profilepic_processor(request):
+    navbar_profile_pic = ""
     if request.user.is_authenticated:
         navbar_profile_pic = BlogAuthor.objects.get(user=request.user)
     return {
