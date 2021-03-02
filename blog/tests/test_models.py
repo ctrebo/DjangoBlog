5aface118dbb1dbb109478075cac465214e7356e
Trebo Christoffer <christoffer.trebo@gmail.com>
2021-03-02 17:36:35 +0100

    add tests for models

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -24,17 +24,91 @@ class BlogAuthorTest(TestCase):
         self.assertEqual(field_label, "prof picture")
 
     def test_bio_max_length(self):
-        author = Author.objects.get(id=1)
+        blog_author = BlogAuthor.objects.get(id=1)
         max_length = blog_author._meta.get_field('bio').max_length
         self.assertEqual(max_length, 400)    
 
 
-    def test_object_name_is_last_name_comma_first_name(self):
-        author = Author.objects.get(id=1)
-        expected_object_name = f'{author.last_name}, {author.first_name}'
-        self.assertEqual(expected_object_name, str(author))
+    def test_object_name_is_user_name(self):
+        blog_author = BlogAuthor.objects.get(id=1)
+        expected_object_name = f'{blog_author.user.username}'
+        self.assertEqual(expected_object_name, str(blog_author))
 
     def test_get_absolute_url(self):
-        author = Author.objects.get(id=1)
+        blog_author = BlogAuthor.objects.get(id=1)
         # This will also fail if the urlconf is not defined.
-        self.assertEqual(author.get_absolute_url(), '/catalog/author/1')
+        self.assertEqual(blog_author.get_absolute_url(), '/blog/bloggers/1')
+
+
+class BlogTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user=User.objects.create_user('foo', password='bar')
+        blog_author = BlogAuthor.objects.create(user=user, bio="My bio")
+        blog = Blog.objects.create(name="test blog", author=blog_author, description="The description is the desctiption")
+
+    
+    def test_name_label(self):
+        blog = Blog.objects.get(id=1)
+        field_label = blog._meta.get_field("name").verbose_name
+        self.assertEqual(field_label, "name")
+    
+    def test_author_label(self):
+        blog = Blog.objects.get(id=1)
+        field_label = blog._meta.get_field("author").verbose_name
+        self.assertEqual(field_label, "author")
+
+    def test_bio_label(self):
+        blog = Blog.objects.get(id=1)
+        field_label = blog._meta.get_field("description").verbose_name
+        self.assertEqual(field_label, "description")
+
+    def test_name_max_length(self):
+        blog = Blog.objects.get(id=1)
+        max_length = blog._meta.get_field('name').max_length
+        self.assertEqual(max_length, 200) 
+
+    def test_description_max_length(self):
+        blog = Blog.objects.get(id=1)
+        max_length = blog._meta.get_field('description').max_length
+        self.assertEqual(max_length, 2000)
+
+    def test_object_name_is_blog_name(self):
+        blog = Blog.objects.get(id=1)
+        expected_object_name = f'{blog.name}'
+        self.assertEqual(expected_object_name, str(blog))
+
+    def test_get_absolute_url(self):
+        blog = Blog.objects.get(id=1)
+        # This will also fail if the urlconf is not defined.
+        self.assertEqual(blog.get_absolute_url(), '/blog/blogs/1') 
+
+class TestBlogComment(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        user=User.objects.create_user('foo', password='bar')
+        blog_author = BlogAuthor.objects.create(user=user, bio="My bio")
+        blog = Blog.objects.create(name="test blog", author=blog_author, description="The description is the desctiption")
+        blog_comment = BlogComment(description="Blog description", author=user, blog=blog)
+    
+    def test_description_label(self):
+        blog_comment = BlogComment.objects.get(id=1)
+        field_label = blog_comment._meta.get_field("description").verbose_name
+        self.assertEqual(field_label, "description")
+
+    def test_author_label(self):
+        blog_comment = BlogComment.objects.get(id=1)
+        field_label = blog_comment._meta.get_field("author").verbose_name
+        self.assertEqual(field_label, "author")
+
+    def test_blog_label(self):
+        blog_comment = BlogComment.objects.get(id=1)
+        field_label = blog_comment._meta.get_field("blog").verbose_name
+        self.assertEqual(field_label, "blog")
+
+    def test_description_max_length(self):
+        blog = Blog.objects.get(id=1)
+        max_length = blog._meta.get_field('description').max_length
+        self.assertEqual(max_length, 1000)
+
+
