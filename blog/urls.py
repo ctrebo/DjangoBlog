cee241aca4220a660cb58fae95805e23834e8ac3
Trebo Christoffer <christoffer.trebo@gmail.com>
2021-03-06 18:53:05 +0100

    update urlpatterns with regex matches

--- blog/urls.py
+++ blog/urls.py
@@ -4,19 +4,19 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path("blogs/", views.BlogListView.as_view(), name="blogs"),
-    path("blogs/<int:pk>", views.BlogDetailView.as_view(), name="blog-detail"),
-    path("bloggers/", views.BlogAuthorListView.as_view(), name="bloggers"),
-    path("bloggers/<int:pk>", views.BlogListbyAuthorView.as_view(), name="blogger-detail"),
-    path("<int:pk>/create/", views.BlogCommentCreate.as_view(), name="create-comment"),
-    path("comment/<int:pk>/delete/", views.BlogCommentDelete.as_view(), name="delete-comment"),
-    path("blog/<int:pk>/update/", views.BlogUpdate.as_view(), name="update-blog"),
-    path("blog/<int:pk>/delete/", views.BlogDelete.as_view(), name="delete-blog"),
-    path("bloggers/<int:pk>/update/", views.BlogAuthorUpdate.as_view(), name="update-blogauthor"),
-    path("profilpage/", views.ProfilPageListView.as_view(), name="profpage-user"),
-    path("newblogs/", views.SeeNewBlogsListView.as_view(), name="new-blogs"),
-    path("blog/create/", views.BlogCreate.as_view(), name="create-blog"),
+    url(r'^$', views.index, name="index"),
+    url(r"^blogs/$", views.BlogListView.as_view(), name="blogs"),
+    url(r"^blogs/(?P<pk>[0-9]+)$", views.BlogDetailView.as_view(), name="blog-detail"),
+    url(r"^bloggers/$", views.BlogAuthorListView.as_view(), name="bloggers"),
+    url(r"^bloggers/(?P<pk>[0-9]+)$", views.BlogListbyAuthorView.as_view(), name="blogger-detail"),
+    url(r"^(?P<pk>[0-9]+)/create/$", views.BlogCommentCreate.as_view(), name="create-comment"),
+    url(r"^comment/(?P<pk>[0-9]+)/delete/$", views.BlogCommentDelete.as_view(), name="delete-comment"),
+    url(r"^blog/(?P<pk>[0-9]+)/update/$", views.BlogUpdate.as_view(), name="update-blog"),
+    url(r"^blog/(?P<pk>[0-9]+)/delete/$", views.BlogDelete.as_view(), name="delete-blog"),
+    url(r"^bloggers/(?P<pk>[0-9]+)/update/$", views.BlogAuthorUpdate.as_view(), name="update-blogauthor"),
+    url(r"^profilpage/$", views.ProfilPageListView.as_view(), name="profpage-user"),
+    url(r"^newblogs/$", views.SeeNewBlogsListView.as_view(), name="new-blogs"),
+    url(r"^blog/create/$", views.BlogCreate.as_view(), name="create-blog"),
     url(r'^signup/$', views.signup, name='signup'),
 
 ]
